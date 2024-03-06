ac647cf46ef85e02399babfd1b533d4c11e01ac7
Tariq Patel <tariqpatel9@gmail.com>
2024-03-06 20:18:49 +0200

    Initial project creation

---
+++ ClearScore/ClearScore/ClearScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearScoreApp.swift
+//  ClearScore
+//
+//  Created by Tasneem Patel on 2024/03/06.
+//
+
+import SwiftUI
+
+@main
+struct ClearScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
